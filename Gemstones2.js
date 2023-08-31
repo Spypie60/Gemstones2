f4533e21e6723d58c7bfb4e304ce0ec0f94e9c93
Spypie60 <117548673+Spypie60@users.noreply.github.com>
2023-08-31 16:07:41 -0400

    Add files via upload

---
+++ Gemstones2.js
@@ -0,0 +1,31 @@
+elements.Ralsf = {
+  color: ["#ff0000","#f44336","#990000","#ea9999","#f4cccc"],
+  behavior: behaviors.POWDER,
+  category: "Powders",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["Onyx"],
+};
+elements.Eslaff = {
+  color: ["#2986cc","#16537e","#0086ff","#9da0f7","#54aeff"],
+  behavior: POWDERS,
+  category: "Powders",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["Diamond"],
+};
+elements.salp = {
+  color: ["#6aa84f","#8fce00","#7cc95a","#8fce00","#c4dabd"],
+  behavior: behaviors.POWDER,
+  category: "Powders",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["Ralsf"],
+},
+  
